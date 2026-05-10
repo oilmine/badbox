a308ce33da558a7db0a95810bfe65d13aa50a5d9
oilmine <investmentslips2@gmail.com>
2026-05-10 19:31:30 +0530

    cat cmd

---
+++ cat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "File not specified.");
+        return 1;
+    }
+
+    char buffer[1024];
+    char *pFileName = argv[1];
+    FILE *pFile = fopen(pFileName, "r");
+    
+    if (pFile == NULL) {
+        fprintf(stderr, "No such file.");
+        return 1;
+    }
+
+    printf("\n");
+    while (fgets(buffer, sizeof(buffer), pFile) != NULL) {
+        printf("%s", buffer);
+    }
+
+    fclose(pFile);
+    return 0;
+}
