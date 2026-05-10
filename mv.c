c6715f82653048bbe378fe518ce176c48cc03180
oilmine <investmentslips2@gmail.com>
2026-05-10 19:51:01 +0530

    mv (move) cmd

---
+++ mv.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("File not specified.\n");
+        return 1;
+    }
+    if (argc < 3) {
+        printf("Destination not specified.\n");
+        return 1;
+    }
+
+    char buffer[1024] = {0};
+    FILE *pSFile = fopen(argv[1], "r");
+    if (pSFile == NULL) {
+        fprintf(stderr, "No such file.");
+        return 1;
+    }
+
+    FILE *pDFile = fopen(argv[2], "w");
+    while (fgets(buffer, sizeof(buffer), pSFile) != NULL) {
+        fprintf(pDFile, "%s", buffer);
+    }
+
+    fclose(pSFile);
+    remove(argv[1]);
+    fclose(pDFile);
+}
