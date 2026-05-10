ce090342347029ca134e8371972e056cff3dcbf1
oilmine <investmentslips2@gmail.com>
2026-05-10 19:55:37 +0530

    touch cmd

---
+++ touch.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("File not specified.\n");
+        return 1;
+    }
+
+    FILE *pFile = fopen(argv[1], "w");
+    if (pFile == NULL) {
+        return 1;
+    }
+
+    fclose(pFile);
+}
